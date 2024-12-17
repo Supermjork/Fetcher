f13396f56b6ef3484c8f4fe91864007d6a94d7f6
Supermjork <markehabaziz@gmail.com>
2024-12-17 19:31:31 +0200

    Main now runs GUI

--- app/main.py
+++ app/main.py
@@ -1,7 +1,19 @@
-# lol, hi
+# --- Installation Shennanigains ---
 from pack_installer import install_nltk_spacy
 
 install_nltk_spacy()
 
+# --- Hopefully it installed with no errors ---
+
+# --- Load the preprocessing stuff ---
 from preprocessing import preprocess
+
+# testing
 print(preprocess("it's ya boi, big mjørkurion, here with u <3"))
+
+# --- GUI? ---
+from gui import IR_GUI
+
+app = IR_GUI()
+
+app.mainloop()
