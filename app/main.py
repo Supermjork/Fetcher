21744b7cd37d91bee5a024397ef511a3179af236
Supermjork <markehabaziz@gmail.com>
2024-12-17 18:41:50 +0200

    And the main big boi

---
+++ app/main.py
@@ -0,0 +1,7 @@
+# lol, hi
+from pack_installer import install_nltk_spacy
+
+install_nltk_spacy()
+
+from preprocessing import preprocess
+print(preprocess("it's ya boi, big mjørkurion, here with u <3"))
