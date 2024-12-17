74e67e0963c695f2de1abd125462b2bc4513e6c1
Supermjork <markehabaziz@gmail.com>
2024-12-17 18:41:22 +0200

    My de-facto standard of preprocessing (Don't have high hopes)

---
+++ app/preprocessing.py
@@ -0,0 +1,38 @@
+import re
+import nltk
+import spacy
+
+stopwords = set(nltk.corpus.stopwords.words("english"))
+
+lang_model = spacy.load("en_core_web_sm")
+
+def regex_text(text: str) -> str:
+    '''
+    Cleans any passed string.
+    Includes accented characters
+    '''
+    text = text.lower()
+
+    regex_patterns = [
+        (r"https?://\S+", ""),    # Remove http or https links
+        (r"\S+@\S+\.\S+", " "),   # Remove email addresses
+        (r"[^\w\sÀ-ÿ'’]", " "),   # Remove special characters, except accented ones
+        (r"\s+", " "),            # Replace multiple spaces with a single space
+        (r"^\s+|\s+$", "")        # Strip leading and trailing spaces
+    ]
+
+    for pattern, replacement in regex_patterns:
+        text = re.sub(pattern, replacement, text)
+    return text
+
+def stopword_removal(text: str, stopwords_set: set) -> str:
+    words = text.split()
+    return " ".join([word for word in words if word not in stopwords_set])
+
+def lemma(tokens, language_model):
+    doc = language_model(tokens)
+
+    return [token.lemma_ for token in doc]
+
+def preprocess(text: str) -> list[str]:
+    return lemma(stopword_removal(regex_text(text), stopwords), lang_model)
