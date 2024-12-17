15093406e24711be35f45bc8611f0e8e0bd55721
Supermjork <markehabaziz@gmail.com>
2024-12-17 19:30:55 +0200

    SpaCy now doesn't reinstall ERRYTAIM

--- app/pack_installer.py
+++ app/pack_installer.py
@@ -39,4 +39,10 @@ def install_nltk_spacy():
     # Won't bother error checking tbh
     # path for curiousity
     # "venv_ir\Lib\site-packages\en_core_web_sm\en_core_web_sm-3.8.0"
-    spacy.cli.download("en_core_web_sm")
+    try:
+        loaded_model = spacy.load("en_core_web_sm")
+        print(f"SpaCy's \"en_core_web_sm\" model is at: {loaded_model._path}")
+        del loaded_model
+    except OSError:
+        spacy.cli.download("en_core_web_sm")
+        print(f"SpaCy downloaded \"en_core_web_sm\" at: {spacy.load("en_core_web_sm")._path}")
