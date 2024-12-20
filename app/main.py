c14008d76c391ee269823c237ec4c56d62bb2323
Supermjork <markehabaziz@gmail.com>
2024-12-20 20:15:55 +0200

    Testing boolean model

--- app/main.py
+++ app/main.py
@@ -6,19 +6,27 @@
 # --- Hopefully it installed with no errors ---
 
 # --- VSM testing ---
-from tfidf_fn import doc_to_dict
+from tfidf_fn import doc_to_dict, doc_processor
 docs_ig = doc_to_dict("data/*.txt")
+docs_processed = doc_processor(docs_ig)
 
 # --- VSM ---
-from retrieval_models import VectorSpaceModel
+from retrieval_models import VectorSpaceModel, BooleanIR
 
 # pls work
-myvsm = VectorSpaceModel(docs_ig)
-query = "computer"
-top_5_v1 = myvsm.return_top_n(query, 5)
-print(f"Top 5 for {query} query: \n")
-for i, doc_score in enumerate(top_5_v1):
-    print(f"{i + 1}. Score of: {doc_score[1]:.3f}. Document Being: \'{doc_score[0]}\'")
+#myvsm = VectorSpaceModel(docs_ig)
+#query = "computer"
+#top_5_v1 = myvsm.return_top_n(query, 5)
+#print(f"Top 5 for {query} query: \n")
+#for i, doc_score in enumerate(top_5_v1):
+#    print(f"{i + 1}. Score of: {doc_score[1]:.3f}. Document Being: \'{doc_score[0]}\'")
+
+mybool = BooleanIR(docs_processed)
+
+result = mybool.search("computer or screen")
+
+print(result)
+
 
 # --- GUI? ---
 # Swapping out CustomTKinter for TTKBootStrap
